69cb8091322be217600956010e552c10732aef8f
AgentDurp <135398342+AgentDurp@users.noreply.github.com>
2024-10-29 13:14:54 -0700

    Add files via upload

--- PS4C/Driver.java
+++ PS4C/Driver.java
@@ -1,5 +1,6 @@
  class Driver{
      public static void main(String[] args){
+         System.out.println(Review.fakeReview("Review0.txt",2));
          System.out.println(Review.totalSentiment("Review0.txt"));
      }
  }
