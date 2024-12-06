f91b77bb79ca61e12ea265bd4aeb9715981c6f9f
AgentDurp <135398342+AgentDurp@users.noreply.github.com>
2024-12-06 14:18:01 -0800

    Add files via upload

--- PS8A/Driver.java
+++ PS8A/Driver.java
@@ -1,19 +1,20 @@
+/*AP CSA
+ *Christopher Orban, Dec 6th 2024
+ * 
+ */
+
 import java.util.Scanner;
 public class Driver{
     public static void main(String[] args){
         int player=-1;
         int won=0;
-        String[][] nums=new String[3][3];
-        for(int i=0;i<nums.length;i++){
-            for(int z=0;z<nums[0].length;z++){
-                nums[i][z]=" ";
-            }
-        }
-        while(won==0){
+        String[][] nums=new String[3][3];//the board
+        nums=initializeBoard(nums);
+        while(won==0){//plays the game. not much point in making this another method imo
             won=checkWinState(nums);
             printBoard(nums);
             player=playerIn(nums,player);
-            if(won!=0){
+            if(won!=0){//if a player has won, stop playing
                 break;
             }
         }
@@ -21,6 +22,7 @@ public class Driver{
     }
 
     public static void printBoard(String[][] nums){
+        //prints the current board state
         for(int row=0;row<nums.length;row++){
             for(int col=0;col<nums[0].length;col++){
                 System.out.print("["+nums[row][col]+"]");
@@ -30,6 +32,8 @@ public class Driver{
     }
 
     public static int playerIn(String[][] nums, int player){
+        //takes player coordinate input and places an X or O depending on whos turn it is 
+        //returns which player it is so that the main method knows 
         Scanner in=new Scanner(System.in);
         String input=in.nextLine();
         String[] split=input.split(",");
@@ -38,6 +42,7 @@ public class Driver{
             coords[i]=Integer.parseInt(split[i]);
         }
         while(nums[coords[0]][coords[1]].equals(" ")){
+
             if(player==-1){
                 nums[coords[0]][coords[1]]="X";
                 player*=-1;
@@ -51,7 +56,9 @@ public class Driver{
 
     public static int checkWinState(String[][] nums){
         int won=0;
-        for(int row=0;row<nums.length;row++){
+        //won represents the player who has won, if its 0 the game continues, if 1 X's one, if 2 O's won
+        //returns int won
+        for(int row=0;row<nums.length;row++){//a loop to check vertical and horizontal win conditions
             if(nums[row][0].equals(nums[row][1])&&nums[row][1].equals(nums[row][2])){
                 if(!nums[row][0].equals(" ")&&!nums[row][1].equals(" ")&&!nums[row][2].equals(" ")){
                     if(nums[row][0].equals("X")){
@@ -71,6 +78,7 @@ public class Driver{
                 }
             }
         }
+        //diagonal win check
         if(nums[0][0].equals(nums[1][1])&&nums[1][1].equals(nums[2][2])){
             if(!nums[0][0].equals(" ")&&!nums[1][1].equals(" ")&&!nums[2][2].equals(" ")){
                 if(nums[1][1].equals("X")){
@@ -80,6 +88,7 @@ public class Driver{
                 }
             }
         }
+        //diagonal win check
         if(nums[0][2].equals(nums[1][1])&&nums[1][1].equals(nums[2][0])){
             if(!nums[0][2].equals(" ")&&!nums[1][1].equals(" ")&&!nums[2][0].equals(" ")){
                 if(nums[1][1].equals("X")){
@@ -90,5 +99,15 @@ public class Driver{
             }
         }
         return won;
+    }
+
+    public static String[][] initializeBoard(String[][] nums){
+        //takes a 2d string array returns the array full of spaces
+        for(int i=0;i<nums.length;i++){
+            for(int z=0;z<nums[0].length;z++){
+                nums[i][z]=" ";
+            }
+        }
+        return nums;
     }
 }
